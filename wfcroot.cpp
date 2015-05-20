32434a148e081e1e54e98edef3e9e263342aada1
Vladimir Migashko <migashko@gmail.com>
2015-05-21 01:53:09 +0300

    upd

--- wfcroot.cpp
+++ wfcroot.cpp
@@ -1,7 +1,8 @@
-#include "wfc_startup/startup/startup_package.hpp"
-#include "wfc_config/config/config_package.hpp"
-#include "wfc_core/core/core_package.hpp"
+#include "wfc_server/server/server_package.hpp"
 #include "wfc_logger/logger/logger_package.hpp"
+#include "wfc_core/core/core_package.hpp"
+#include "wfc_config/config/config_package.hpp"
+#include "wfc_startup/startup/startup_package.hpp"
 #include "wfcroot.hpp"
 
 namespace wfc{ 
@@ -13,7 +14,7 @@ wfcroot( std::list< std::shared_ptr<ipackage> > packages)
   packages.push_back( std::make_shared<config_package>() );
   packages.push_back( std::make_shared<core_package>() );
   packages.push_back( std::make_shared<logger_package>() );
-  //std::initializer_list< std::shared_ptr<ipackage> > result({packages});
+  packages.push_back( std::make_shared<server_package>() );
   return packages;
 }
 
