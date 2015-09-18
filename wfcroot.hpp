15e566b5aed0f71030f46b80b18e24751d2ce2ab
Vladimir Migashko <migashko@gmail.com>
2015-09-18 21:16:02 +0300

    upd

--- wfcroot.hpp
+++ wfcroot.hpp
@@ -6,10 +6,6 @@
 
 namespace wfc{ 
   
-/*typedef std::shared_ptr<ipackage> package_ptr;
-  typedef std::initializer_list< package_ptr > packages_initializer;
-*/
-
 std::list< std::shared_ptr<ipackage> >
 wfcroot( std::list< std::shared_ptr<ipackage> > packages);
 
