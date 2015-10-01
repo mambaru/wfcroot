81898d031218be40e6066f1e6fdfd7472aa6fe9c
Vladimir Migashko <migashko@gmail.com>
2015-10-01 20:32:12 +0300

    upd

--- wfcroot.hpp
+++ wfcroot.hpp
@@ -6,6 +6,10 @@
 
 namespace wfc{ 
   
+/**
+ * \defgroup wfcroot Стандартные пакеты WFC
+ */
+  
 std::list< std::shared_ptr<ipackage> >
 wfcroot( std::list< std::shared_ptr<ipackage> > packages);
 
