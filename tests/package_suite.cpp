36100f5c918d2c0aac6e0c493a2ab3b003b8bdbb
Vladimir Migashko <migashko@gmail.com>
2019-03-13 13:35:17 +0300

    initial tests

--- tests/package_suite.cpp
+++ tests/package_suite.cpp
@@ -1,15 +1,17 @@
 #include <fas/testing.hpp>
+#include <wfcroot/wfcroot.hpp>
 
 namespace {
 
-UNIT(init, "")
+UNIT(unit1, "")
 {
   using namespace fas::testing;
-  t << nothing;
+  auto list = wfc::wfcroot({});
+  t << equal<expect, size_t>(list.size(), 3) << FAS_FL;
 }
 
 }
   
 BEGIN_SUITE(package_suite, "")
-  ADD_UNIT( init )
+  ADD_UNIT( unit1 )
 END_SUITE(package_suite)
